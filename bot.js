5e96316d29a52cf7f7890c04d20a06e0246efadb
David Hanlon <davidhanlon23@gmail.com>
2021-07-21 19:53:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,11 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
 
 
 /*
